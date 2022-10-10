f52e3df5aa2ccee1be94f343f2538605e0b5f23c
Grinchenko Sergei <butters.blaga@gmail.com>
2022-10-11 04:16:38 +0900

    feat: create controllers & routes for users

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const usersRouter = require('./routes/users');
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(express.json());
+app.use('/', usersRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
