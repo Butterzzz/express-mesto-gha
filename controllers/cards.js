88abc957c347e88b99970ff6ba2d31986da1f5f0
Grinchenko Sergei <butters.blaga@gmail.com>
2022-10-13 03:38:14 +0900

    feat: add error handling for cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,7 +3,7 @@ const Card = require('../models/card');
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.status(200).send(cards))
-    .catch((err) => res.status(500).send(err));
+    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка.' }));
 };
 
 module.exports.createCard = (req, res) => {
@@ -12,26 +12,57 @@ module.exports.createCard = (req, res) => {
 
   Card.create({ name, link, owner })
     .then((card) => res.status(200).send(card))
-    .catch((err) => res.status(500).send(err));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при создании карточки.' });
+        return;
+      }
+      res.status(500).send({ message: 'На сервере произошла ошибка.' });
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => {
-      if (!card) return res.status(404).send({ message: 'Карточки с таким id не существует' });
-      return res.status(200).send(card);
+      if (!card) {
+        res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+        return;
+      }
+      res.status(200).send(card);
     })
-    .catch((err) => res.status(500).send(err));
+    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка.' }));
 };
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .then((card) => res.status(200).send(card))
-    .catch((err) => res.status(500).send(err));
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
+        return;
+      }
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка.' });
+      }
+      return res.status(500).send({ message: 'На сервере произошла ошибка.' });
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .then((card) => res.status(200).send(card))
-    .catch((err) => res.status(500).send(err));
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
+        return;
+      }
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные для снятии лайка.' });
+      }
+      return res.status(500).send({ message: 'На сервере произошла ошибка.' });
+    });
 };
