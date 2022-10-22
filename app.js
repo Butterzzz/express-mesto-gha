77603cb7898f0840dad3c3a0148dbf7c351af148
Grinchenko Sergei <butters.blaga@gmail.com>
2022-10-23 06:43:10 +0900

    feat: protect API with authorization

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
@@ -33,6 +34,8 @@ app.disable('x-powered-by');
 
 app.post('/signin', login);
 app.post('/signup', createUser);
+
+app.use(auth);
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
 app.use((req, res) => res.status(404).send({ message: 'Страница не найдена' }));
